95361b9ed3aa98ea203997b9239b33d7ca0e392d
MichaelCodes <michael.lang3303@gmail.com>
2025-11-06 22:55:31 -0600

    JS Learning

--- Class_Materials/class10-materials/JS-CrashCourse/main.js
+++ Class_Materials/class10-materials/JS-CrashCourse/main.js
@@ -1,1 +1,12 @@
-alert('Hello World');
+//  alert('Hello World');
+// console.error('This is an error');
+// console.warn('This is a warning');
+
+const name = 'John';
+const age = 30;
+const rating = 4.5;
+const isCool = true;
+const x = null;
+const y = undefined;
+
+console.log(typeof rating);
