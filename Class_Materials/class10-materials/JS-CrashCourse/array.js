2f55ad473399b2524344d163ed62f80b89b564c3
MichaelCodes <michael.lang3303@gmail.com>
2025-11-08 08:27:04 -0600

    Array JS Split

---
+++ Class_Materials/class10-materials/JS-CrashCourse/array.js
@@ -0,0 +1,20 @@
+//  alert('Hello World');
+// console.error('This is an error');
+// console.warn('This is a warning');
+
+const name = 'John';
+const age = 30;
+const rating = 4.5;
+const isCool = true;
+const x = null;
+const y = undefined;
+let z; // This should also result in undefined
+
+console.log(typeof rating);
+
+const fruits = ['apple', 'orange', 'pear'];
+console.log(fruits);
+
+/*
+Mult-line Comment
+*/
