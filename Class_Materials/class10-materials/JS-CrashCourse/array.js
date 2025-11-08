50f4def8af0f7fa6e6296a32b3d3636bf3a668e3
MichaelCodes <michael.lang3303@gmail.com>
2025-11-08 08:31:19 -0600

    Main Commits

--- Class_Materials/class10-materials/JS-CrashCourse/array.js
+++ Class_Materials/class10-materials/JS-CrashCourse/array.js
@@ -13,6 +13,26 @@ let z; // This should also result in undefined
 console.log(typeof rating);
 
 const fruits = ['apple', 'orange', 'pear'];
+
+/*
+Mult-line Comment
+*/
+
+//  alert('Hello World');
+// console.error('This is an error');
+// console.warn('This is a warning');
+
+
+
+console.log(typeof rating);
+
+fruits[3] = 'grapes';
+fruits.push('mangos');
+fruits.unshift('strawberries');
+fruits.pop()
+
+console.log(Array.isArray('fruits'));
+console.log(fruits.indexOf('oranges'))
 console.log(fruits);
 
 /*
