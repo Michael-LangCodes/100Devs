623c72b85431faf3a9c05f2b8f219d9218999e2a
MichaelCodes <michael.lang3303@gmail.com>
2025-11-09 09:05:59 -0600

    JS Dictionary

--- Class_Materials/class10-materials/JS-CrashCourse/main.js
+++ Class_Materials/class10-materials/JS-CrashCourse/main.js
@@ -2,5 +2,15 @@ const person = {
     firstName: 'John',
     lastName: 'Doe',
     age: 30,
-    hobbies: ['music','movies','sports']
+    hobbies: ['music','movies','sports'],
+    address: {
+        street: '50 main st',
+        city: 'Boston',
+        state: 'MA'
+    }
 }
+
+
+alert(person)
+console.log(person)
+console.log(person.firstName,person.lastName);
