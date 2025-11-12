14f453eb2da627fbb06ca18c16d5ede58b1cc7bd
MichaelCodes <michael.lang3303@gmail.com>
2025-11-12 15:15:17 -0600

    main updates

--- Class_Materials/class10-materials/JS-CrashCourse/methods.js
+++ Class_Materials/class10-materials/JS-CrashCourse/methods.js
@@ -8,3 +8,12 @@ ul.lastElementChild.innerHTML = '<h4>Hello</h4>';
 
 const btn = document.querySelector('.btn');
 btn.style.background = 'red';
+
+btn.addEventListener('click', (e) => {
+    e.preventDefault();
+    document.querySelector('#my-form')
+    .style.background = '#ccc';
+    document.querySelector('body').classList.add('bg-dark');
+    document.querySelector('.items')
+    .lastElementChild.innerHTML = '<h1>Hello</h1>'
+})
