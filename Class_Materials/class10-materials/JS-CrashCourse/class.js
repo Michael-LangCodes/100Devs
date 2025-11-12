a1c3b0ee5d5d82f12b06d6418bb09173dbe9d890
MichaelCodes <michael.lang3303@gmail.com>
2025-11-11 20:05:24 -0600

    OOP Class Creation

---
+++ Class_Materials/class10-materials/JS-CrashCourse/class.js
@@ -0,0 +1,29 @@
+//Class
+class Person {
+    constructor(firstName,lastName,DOB){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.DOB = new Date(DOB);
+    }
+
+    getBirthYear(){
+        return this.DOB.getFullYear();
+    }
+
+    getFullName(){
+        return '${this.firstName} ${this.LastName}';
+    }
+}
+
+
+//Instatiate the Object
+const person1 = new Person('John', 'Doe', '4-3-1980')
+const person2 = new Person('Mary', 'Smih', '3-6-1978')
+
+
+
+console.log(person1)
+console.log(person2.DOB)
+console.log(person2.DOB.getFullYear())
+console.log(person1.getBirthYear())
+console.log(person1.getFullName())
