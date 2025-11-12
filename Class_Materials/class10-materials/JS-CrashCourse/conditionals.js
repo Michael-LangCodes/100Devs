27ef3c6aa1089098dea37e320f19fbad5ca85091
MichaelCodes <michael.lang3303@gmail.com>
2025-11-11 19:21:24 -0600

    Functions

--- Class_Materials/class10-materials/JS-CrashCourse/conditionals.js
+++ Class_Materials/class10-materials/JS-CrashCourse/conditionals.js
@@ -22,3 +22,23 @@ if(x > 5 || y > 10){
 if(x > 5 && y > 10){
     console.log('X is more than 5 and y is more than 10')
 }
+
+
+// ? is the THEN operator
+const color = x > 10 ? 'red': 'blue';
+
+console.log(color);
+
+const color_2 = 'green'
+
+switch(color_2){
+    case 'red':
+        console.log('color is red');
+        break;
+    case 'blue':
+        console.log('color is blue');
+        break;
+    default:
+        console.log('color is NOT red or blue');
+        break;
+}
