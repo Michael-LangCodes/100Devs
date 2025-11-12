bce11c39bfd3a6369de6eb46dc92e5cb785c7ccb
MichaelCodes <michael.lang3303@gmail.com>
2025-11-11 23:02:45 -0600

    Selectors

---
+++ Class_Materials/class10-materials/JS-CrashCourse/main.js
@@ -0,0 +1,13 @@
+//Window is the top level object
+console.log(window);
+
+//Single element selectors (selects the first element if more than one)
+const form = document.getElementById('my-form');
+console.log(form);
+
+const form2 = document.querySelector('h1');
+console.log(form2);
+
+//Multiple element selectors
+item = document.querySelectorAll('.item');
+console.log(item)
