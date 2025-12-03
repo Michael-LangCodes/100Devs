92940b89680f86311ad8146783ef7d56aebca1c4
MichaelCodes <michael.lang3303@gmail.com>
2025-12-03 06:41:13 -0600

    Day Selector Conditional Logic

--- Class_Materials/class12-materials/class12-materials/class-weekend-boring/js/main.js
+++ Class_Materials/class12-materials/class12-materials/class-weekend-boring/js/main.js
@@ -4,7 +4,14 @@ function check() {
 
   const day = document.querySelector('#day').value
 
-  //Conditionals go here
-
-
+  if (day === 'Thursday' || day === 'Tuesday'){
+    text = "Today is Class Day"
+    document.querySelector('#placeToSee').innerHTML = text
+  }else if (day === 'Saturday' || day === 'Sunday'){
+    text = "Today is Weekend"
+    document.querySelector('#placeToSee').innerHTML = text
+  }else{
+    text = "Boring"
+    document.querySelector('#placeToSee').innerHTML = text
+  }
 }
