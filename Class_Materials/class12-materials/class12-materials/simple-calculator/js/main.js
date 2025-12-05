f6cadd73da7c0e83971ac281640cf5a0ce715cb1
MichaelCodes <michael.lang3303@gmail.com>
2025-12-04 20:52:27 -0600

    Calculator Additions

--- Class_Materials/class12-materials/class12-materials/simple-calculator/js/main.js
+++ Class_Materials/class12-materials/class12-materials/simple-calculator/js/main.js
@@ -2,14 +2,26 @@ let total = 0
 
 document.querySelector('#pumpkin').addEventListener('click', pumpkin)
 document.querySelector('#dominosPizza').addEventListener('click', domino)
-
+document.querySelector('#zebra').addEventListener('click', zebra)
+document.querySelector('#cantThinkOfAnything').addEventListener('click', cant)
 
 function pumpkin(){
     total = 0
     document.querySelector('#placeToPutResult').innerText = total
+    document.querySelector('body').style.backgroundColor = 'blue'
 }
 
 function domino(){
     total = total +3
+    document.querySelector('#placeToPutResult').innerText = total
+}
+
+function zebra(){
+    total = total +9
+    document.querySelector('#placeToPutResult').innerText = total
+}
+
+function cant(){
+    total = total -2
     document.querySelector('#placeToPutResult').innerText = total
 }
