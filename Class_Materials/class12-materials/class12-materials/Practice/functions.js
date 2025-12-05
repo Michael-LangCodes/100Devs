549b5e73e9d2768e2bea300e68d51b1c6988d074
MichaelCodes <michael.lang3303@gmail.com>
2025-12-04 20:48:11 -0600

    Fucntions

--- Class_Materials/class12-materials/class12-materials/Practice/functions.js
+++ Class_Materials/class12-materials/class12-materials/Practice/functions.js
@@ -7,5 +7,27 @@ function checkAge(age) {
 }
 
 function checkAgeTernary(age) {
-  age>18? True: False
+  return (age>18)? true: confirm('Did parents allow you?')
+}
+
+function checkAge(age) {
+  return (age > 18) || confirm('Did parents allow you?');
+}
+
+function min(a,b) {
+    if (a > b) {
+        return b;
+    } else {
+        return a;
+    }
+}
+
+function pow(x,n) {
+    let result = x;
+
+    for (let i = 1; i < n; i++) {
+        result *= x;
+    }
+
+    return result;
 }
