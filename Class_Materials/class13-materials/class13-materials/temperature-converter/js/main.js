b22199b395d28d7478aa5bde680e034bfee0c6ef
MichaelCodes <michael.lang3303@gmail.com>
2025-12-07 08:02:38 -0600

    Temp Converter JS Changes

--- Class_Materials/class13-materials/class13-materials/temperature-converter/js/main.js
+++ Class_Materials/class13-materials/class13-materials/temperature-converter/js/main.js
@@ -1,7 +1,12 @@
 //Write your pseduo code first! 
 
 //Get the input temperature
-document.querySelector('#CelsiusTemp')
+document.querySelector('#convert').addEventListener('click', run)
+let in_temp = document.querySelector('#CelsiusTemp').value
 //Convert
-
+function run(){
+    let in_temp = document.querySelector('#CelsiusTemp').value
+    let output_temp = (9/5)*in_temp + 32
+    document.querySelector('#placeToDisplay').innerText = `${output_temp}`
+}
 //Output value
