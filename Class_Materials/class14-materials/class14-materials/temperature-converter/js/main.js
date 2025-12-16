f4b1d64523901563f4f4b9514dc5c42332e0cdc6
MichaelCodes <michael.lang3303@gmail.com>
2025-12-15 21:41:12 -0600

    Temp Converter

--- Class_Materials/class14-materials/class14-materials/temperature-converter/js/main.js
+++ Class_Materials/class14-materials/class14-materials/temperature-converter/js/main.js
@@ -1,1 +1,12 @@
 //Write your pseduo code first! 
+
+document.querySelector('#convert').addEventListener('click', convert)
+
+function convert(){
+    //take the Input Code
+    const inputTemp = document.querySelector('#inputTemp').value
+    //convert to celsius
+    const outTemp = inputTemp*(9/5) + 32
+    //output the code
+    document.querySelector('#placeToDisplay').innerText = `${outTemp}`
+}
