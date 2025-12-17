286780ae4b78670c0b2541f9a1f4d294e0b011b7
MichaelCodes <michael.lang3303@gmail.com>
2025-12-16 19:32:15 -0600

    Functions Commits

--- Class_Materials/class14-materials/class14-materials/simple-functions/js/main.js
+++ Class_Materials/class14-materials/class14-materials/simple-functions/js/main.js
@@ -1,5 +1,23 @@
 //create a function that adds two numbers and alerts the sum
+function addTwoNumbers(num1, num2){
+    const output = num1+num2;
+    alert(output)
+}
+
+addTwoNumbers(2,3)
 
 //create a function that multiplys three numbers and console logs the product
+function multiplyThreeNumbers(num1, num2, num3){
+    const output= num1*num2*num3;
+    console.log(output)
+}
+
+multiplyThreeNumbers(1,2,3)
 
 //create a function that divides two numbers and returns the ???
+function divideTwoNumbers(num1,num2){
+    const output = num1/num2;
+    return output
+}
+
+console.log(divideTwoNumbers(6,3))
