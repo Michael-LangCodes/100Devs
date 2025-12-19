87f8a75a4a658d4865fdf3880488539a058416b5
MichaelCodes <michael.lang3303@gmail.com>
2025-12-18 23:07:21 -0600

    While Loop Commit

--- Class_Materials/class16-materials/class16-materials/stop-snacking/js/main.js
+++ Class_Materials/class16-materials/class16-materials/stop-snacking/js/main.js
@@ -1,1 +1,12 @@
 //Create a function that grabs the number of snacks from the input and tells you to stop that many times
+document.querySelector('#help').addEventListener('click', stop)
+
+function stop(){
+    const numOfSnacks = Number(document.querySelector('input').value)
+    document.querySelector('#stops').innerText = ''
+    let i = 0
+    while(i<numOfSnacks){
+        document.querySelector('#stops').innerText += ' stop'
+        i++
+    }
+}
