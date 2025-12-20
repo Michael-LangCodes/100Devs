4a5fdb99af483ec7d324c12d70a0b6aac5c428b9
MichaelCodes <michael.lang3303@gmail.com>
2025-12-20 11:14:24 -0600

    JSWay Chapter 2 JS Commits

--- Class_Materials/class16-materials/class16-materials/thejsway/chapter2/chapter2.js
+++ Class_Materials/class16-materials/class16-materials/thejsway/chapter2/chapter2.js
@@ -1,1 +1,36 @@
+//Improved Hello
+let names = prompt("Enter First and Last Name: ");
+console.log(names);
 
+//Final Value Prediction
+let a = 2; //2
+a -= 1; //1
+a++; //2
+let b = 8; //8
+b += 2; //10
+const c = a + b * b; //102
+const d = a * b + b; //30
+const e = a * (b + b); //40
+const f = a * b / a; //10
+const g = b / a * a; //10
+console.log(a, b, c, d, e, f, g); //2, 10, 102, 30, 40, 10, 10
+
+//VAT Calc
+let price = prompt('What is the price?');
+price = 1.206*price;
+console.log(price)
+
+//Temp Conversion
+let temp = prompt('Input Temperature:');
+temp = temp*(9/5) +32;
+console.log(temp);
+
+//Variable Swap
+let number1 = 5;
+let number2 = 3;
+
+number1 = number2;
+number2 = 5;
+
+console.log(number1); // Should show 3
+console.log(number2); // Should show 5
