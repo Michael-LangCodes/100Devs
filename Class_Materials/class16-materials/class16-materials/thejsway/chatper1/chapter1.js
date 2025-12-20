6b3cdeb3e062b7c1d1902e55ee6921a1eac69560
MichaelCodes <michael.lang3303@gmail.com>
2025-12-20 10:50:47 -0600

    JSWay Chapter 1

---
+++ Class_Materials/class16-materials/class16-materials/thejsway/chatper1/chapter1.js
@@ -0,0 +1,14 @@
+//Presentation
+console.log("Michael");
+console.log(24);
+
+//Minimal Calculator
+console.log(6+3);
+console.log(6-3);
+console.log(6*3);
+console.log(6/3);
+
+//Value Prediction
+console.log(4 + 5); //9
+console.log("4 + 5"); //4 + 5
+console.log("4" + "5"); //45
