ff88d8ea11a63dfb4474a9fb8b5e1f2df388d357
MichaelCodes <michael.lang3303@gmail.com>
2025-12-22 10:56:34 -0600

    JSWay Chapter 4

--- Class_Materials/class16-materials/class16-materials/thejsway/chapter4/chapter4.js
+++ Class_Materials/class16-materials/class16-materials/thejsway/chapter4/chapter4.js
@@ -14,7 +14,38 @@ for (let i = numInput; i <= numInput+10; i++) {
 
 //Input Validation
 let numCheck = prompt("Enter a Number:");
-while(numCheck < 100){
+while(numCheck > 50){
     numCheck = prompt("Enter a Number:");
 }
 console.log("This is the Number: " + numCheck)
+
+//Multiplication Table
+let multiplicationNumber = prompt("Enter a Multiplication Number");
+if (multiplicationNumber>2 || multiplicationNumber<9){
+    for (i=1; i<13; i++){
+        mathOutput = i*multiplicationNumber;
+        console.log(`${multiplicationNumber}*${i} = ${mathOutput}`)
+    }
+}
+
+//Neither yes nor no
+let userInput = "default";
+while(userInput != 'yes' && userInput != 'no'){
+    console.log(userInput)
+    userInput = prompt('Enter Text Here:').toLowerCase();
+}
+
+//FizzBuzz
+for (i=1; i<=100; i++){
+    let analyzeValue3 = i%3;
+    let analyzeValue5 = i%5;
+    let outputValue = i;
+    if(analyzeValue3 === 0 && analyzeValue5 === 0){
+        outputValue = "FizzBuzz"
+    }else if (analyzeValue3 === 0){
+        outputValue = "Fizz"
+    }else if (analyzeValue5 === 0){
+        outputValue = "Buzz"
+    }
+    console.log(outputValue)
+}
