d738e9a2808452ed9478ab058144276d9647e730
MichaelCodes <michael.lang3303@gmail.com>
2025-12-25 19:42:25 -0600

    Arrat Creation Function

--- Class_Materials/class19-materials/class19-materials/arrays-worldwide-cheersmack/js/main.js
+++ Class_Materials/class19-materials/class19-materials/arrays-worldwide-cheersmack/js/main.js
@@ -1,1 +1,9 @@
 //Create a function that takes in a number. The function should return an array that contains every number from 1 to that number as seperate elements
+function arrayToNum(num){
+    let list = [];
+    for(let i = 0; i < num; i++){
+        list[i] = i+1;
+    }
+    console.log(list)
+}
+arrayToNum(15);
