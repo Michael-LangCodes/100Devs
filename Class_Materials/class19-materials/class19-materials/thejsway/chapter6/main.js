c0b9b3a9cb9599c1528ab63fb49548cf6679fea5
MichaelCodes <michael.lang3303@gmail.com>
2025-12-29 16:09:41 -0600

    Chapter 6 Commit

--- Class_Materials/class19-materials/class19-materials/thejsway/chapter6/main.js
+++ Class_Materials/class19-materials/class19-materials/thejsway/chapter6/main.js
@@ -35,3 +35,37 @@ const dog = {
 
 console.log(`${dog.name} is a ${dog.species} dog measuring ${dog.size}`);
 console.log(`Look, a cat! ${dog.name} barks: ${dog.bark()}`);
+
+//Modeling a Circle
+const r = Number(prompt("Enter the circle radius:"));
+
+// TODO: create the circle object here
+const circle = {
+    radius: r,
+    circumference(){
+        return 2*Math.PI*r
+    },
+    area(){
+        return Math.PI*r**2
+    }
+};
+
+console.log(`Its circumference is ${circle.circumference()}`);
+console.log(`Its area is ${circle.area()}`);
+
+//Modeling a Bank
+const account = {
+    name: "Alex",
+    balance: 0,
+    credit(value){
+        return this.balance += value
+    },
+    describe(){
+        return `owner: ${this.name}, balance ${this.balance}`
+    }
+};
+
+console.log(account.describe());
+account.credit(250);
+account.credit(-80);
+console.log(account.describe());
