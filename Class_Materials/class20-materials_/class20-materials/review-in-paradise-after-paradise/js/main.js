733cd1fb65dc481978d547fb62fb2f35425eccc8
MichaelCodes <michael.lang3303@gmail.com>
2026-01-06 18:04:25 -0600

    paradise after paradise '

--- Class_Materials/class20-materials_/class20-materials/review-in-paradise-after-paradise/js/main.js
+++ Class_Materials/class20-materials_/class20-materials/review-in-paradise-after-paradise/js/main.js
@@ -1,1 +1,12 @@
 // Create a function that takes in an array. If the first number, is less than the last number, alert "Hi". If the first number is greater than the last number, alert "Bye". If they are equal, alert "We close in an hour".
+function arrayCheck(input){
+    if (input[0] < input[input.length-1]){
+        alert("Hi");
+    }else if (input[0] < input[input.length-1]){
+        alert("Hi");
+    }else if (input[0] === input[input.length-1]){
+        alert('We Close In An Hour');
+    }
+}
+arrayInput = [1,23,4,56,7];
+arrayCheck(arrayInput);
