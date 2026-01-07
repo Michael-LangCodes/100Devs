dbd8be78b5d7993ead4ac1570834956fcefc09c5
MichaelCodes <michael.lang3303@gmail.com>
2026-01-06 18:12:02 -0600

    summer games review

--- Class_Materials/class20-materials_/class20-materials/review-summer-games/js/main.js
+++ Class_Materials/class20-materials_/class20-materials/review-summer-games/js/main.js
@@ -1,1 +1,9 @@
 //Create a function that takes in an array of numbers. Multiply each number together and alert the product. 
+function multiplyArray(arr){
+    let total = 1;
+    arr.forEach(x => total *= x)
+    alert(total);
+}
+
+arrayInput = [1,2,3,4,5,6];
+multiplyArray(arrayInput);
