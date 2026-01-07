3fce0f05e521f465b032dab87be6f3ce2ef0bf84
MichaelCodes <michael.lang3303@gmail.com>
2026-01-06 19:41:37 -0600

    arrays-worldwide

--- Class_Materials/class20-materials_/class20-materials/arrays-worldwide-cheersmack/js/main.js
+++ Class_Materials/class20-materials_/class20-materials/arrays-worldwide-cheersmack/js/main.js
@@ -1,1 +1,9 @@
 //Create a function that takes in a number. The function should return an array that contains every number from 1 to that number as seperate elements
+function arrayToNum(num){
+    let output = [];
+    for(let i = 1; i<=num;i++){
+        output.push(i);
+    }
+    console.log(output);
+}
+arrayToNum(5);
