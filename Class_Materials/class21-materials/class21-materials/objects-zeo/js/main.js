be2080ebd3f422dcc135651e77007a44bd0a77a0
MichaelCodes <michael.lang3303@gmail.com>
2026-01-07 19:01:36 -0600

    ZEO

--- Class_Materials/class21-materials/class21-materials/objects-zeo/js/main.js
+++ Class_Materials/class21-materials/class21-materials/objects-zeo/js/main.js
@@ -1,1 +1,18 @@
 //Create a stopwatch object that has four properties and three methods
+let stopwatch = {};
+
+stopwatch.currentTime = 12;
+stopwatch.brand = 'nike'
+stopwatch.color = 'black'
+stopwatch.shape = 'round';
+
+stopwatch.tellTime = function(time){
+    console.log(stopwatch.currentTime)
+}
+
+stopwatch.start = function(){
+    console.log('Starting');
+}
+stopwatch.end = function(){
+    console.log('Ending');
+}
