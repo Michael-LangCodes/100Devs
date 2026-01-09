96da4bdc1a507614bf31be3b3825a2c74b9a66fe
MichaelCodes <michael.lang3303@gmail.com>
2026-01-08 20:03:31 -0600

    Galaxy Commits

--- Class_Materials/class22-materials/class22-materials/objects-lost-galaxy/js/main.js
+++ Class_Materials/class22-materials/class22-materials/objects-lost-galaxy/js/main.js
@@ -1,1 +1,32 @@
 //Create a mouse object that has four properties and three methods
+let mice = {
+    color: 'white',
+    size: 'small',
+    noise: 'squeak',
+    food: 'carrot',
+    talk(){
+        alert(noise);
+    },
+    askForFood(){
+        alert(`Can I have ${this.food}`);
+    },
+    tellColor(){
+        alert(`I am a ${this.color} mouse`);
+    }
+}
+
+let mouse ={};
+mouse.brand = 'Logi';
+mouse.color = 'black';
+mouse.model = 'basic';
+mouse.wireless = true;
+
+mouse.leftClick = function(){
+    console.log('Left Click')
+}
+mouse.rightClick = function(){
+    console.log('Right Click')
+}
+mouse.scroll = function(){
+    console.log('Scrollllinnnggg')
+}
