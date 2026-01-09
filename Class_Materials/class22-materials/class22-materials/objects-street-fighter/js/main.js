3d3c9cb0693ed4806088dbbec9caa59754007fad
MichaelCodes <michael.lang3303@gmail.com>
2026-01-08 21:19:12 -0600

    Street Fighter

--- Class_Materials/class22-materials/class22-materials/objects-street-fighter/js/main.js
+++ Class_Materials/class22-materials/class22-materials/objects-street-fighter/js/main.js
@@ -1,1 +1,16 @@
 //Create a street fighter constructor that makes fighting game characters with 4 properties and 3 methods
+function streetFighter(fighterHealth, fighterDamage, fighterAttack){
+    this.health = fighterHealth;
+    this.damage = fighterDamage;
+    this.attack = fighterAttack;
+    this.other = fighterOther;
+    this.yell = function(){
+        alert('YELLLL')
+    };
+    this.moveLeft = function(){
+        alert('Moving Left')
+    };
+    this.moveRight = function(){
+        alert('Moving Right')
+    };
+}
