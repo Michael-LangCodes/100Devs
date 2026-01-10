0a8a43ca44ddd3fff9f69a39b4c534546bfc43df
MichaelCodes <michael.lang3303@gmail.com>
2026-01-10 16:41:03 -0600

    Lost Galaxy

--- Class_Materials/class23-materials/class23-materials/objects-lost-galaxy/js/main.js
+++ Class_Materials/class23-materials/class23-materials/objects-lost-galaxy/js/main.js
@@ -1,1 +1,16 @@
 //Create a dog object that has four properties and three methods
+let dog = {
+    height: 3,
+    noise: 'bark',
+    color: 'red',
+    tail: 'wag',
+    speak(){
+        console.log(dog.noise)
+    },
+    jump(){
+        console.log('JUMPING')
+    },
+    fetch(){
+        console.log('Fetching')
+    }
+}
