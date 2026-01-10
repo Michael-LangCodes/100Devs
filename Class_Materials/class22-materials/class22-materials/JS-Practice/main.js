d3498fc9fa3fd855e35a9fbb4be3768a9e9874e1
MichaelCodes <michael.lang3303@gmail.com>
2026-01-10 12:31:16 -0600

    Object Practice

--- Class_Materials/class22-materials/class22-materials/JS-Practice/main.js
+++ Class_Materials/class22-materials/class22-materials/JS-Practice/main.js
@@ -7,3 +7,43 @@ delete user.name;
 
 console.log(user);
 
+//Check for empty
+function isEmpty(obj){
+    for (let key in obj){
+        return false;
+    }
+    return true
+}
+sample = {};
+console.log(isEmpty(sample));
+
+//Sum Objects
+let salaries = {
+  John: 100,
+  Ann: 160,
+  Pete: 130
+}
+let total =0;
+for(keys in salaries){
+    total += salaries[keys];
+}
+console.log(total);
+
+//Multiply Numeric by 2
+function multiplyNumeric(input){
+    for(key in input){
+        if (typeof(input[key])==='number'){
+            input[key] *= 2;
+        }
+    }
+    console.log(input);
+}
+
+// before the call
+let menu = {
+  width: 200,
+  height: 300,
+  title: "My menu"
+};
+
+multiplyNumeric(menu);
