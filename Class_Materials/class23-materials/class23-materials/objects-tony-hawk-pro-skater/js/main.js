2a4a7383725ddd4bb6d38b32378497a4c60957b5
MichaelCodes <michael.lang3303@gmail.com>
2026-01-10 16:49:11 -0600

    Tony Hawk

--- Class_Materials/class23-materials/class23-materials/objects-tony-hawk-pro-skater/js/main.js
+++ Class_Materials/class23-materials/class23-materials/objects-tony-hawk-pro-skater/js/main.js
@@ -1,1 +1,18 @@
 //Create a Tony Hawk Pro Skater constructor that makes fighting game characters with 4 properties and 3 methods
+function Skater(trick,jump,speed,emote){
+    this.trick = trick;
+    this.jump = jump;
+    this.speed = speed;
+    this.emote = emote;
+    this.useEmote = function(){
+        console.log(`This is my ${this.emote}`);
+    };
+    this.useTrick = function(){
+        console.log(`Using the ${this.trick}`);
+    };
+    this.taunt = function(){
+        console.log("I'm doing this");
+    }
+}
+
+let tonyHawk = new Skater('kickflip',4,8,'swag');
