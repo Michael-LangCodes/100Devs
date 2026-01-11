1b7bebe46e5844b2775ef304448180c843d52d60
MichaelCodes <michael.lang3303@gmail.com>
2026-01-11 15:42:39 -0600

    Lost Galaxy Objects

--- Class_Materials/class26-materials/class26-materials/objects-lost-galaxy/js/main.js
+++ Class_Materials/class26-materials/class26-materials/objects-lost-galaxy/js/main.js
@@ -1,1 +1,31 @@
-//Create a dog object that has four properties and three methods
+//Create a pizza object that has four properties and three methods
+let pizza ={};
+pizza.toppings = ['pepperoni', 'onions'];
+pizza.cheese = 'colby-jack';
+pizza.crust = 'thin';
+pizza.temp = '400F';
+pizza.cook = function(){
+    console.log(`Cook the pizza at ${pizza.temp}.`);
+}
+pizza.burnMouth = function(){
+    console.log('AHHHH')
+}
+pizza.frisbee = function(){
+    console.log('Throwing');
+}
+
+function MakePizza(toppings,cheese,crust,temp){
+    this.toppings = toppings;
+    this.cheese = cheese;
+    this.crust = crust;
+    this.temp = temp;
+    this.cook = function(){
+    console.log(`Cook the pizza at ${pizza.temp}.`);
+    };
+    this.burn = function(){
+    console.log('AHHHH');
+    };
+    this.frisbee = function(){
+        console.log('Throwing');
+    };
+}
