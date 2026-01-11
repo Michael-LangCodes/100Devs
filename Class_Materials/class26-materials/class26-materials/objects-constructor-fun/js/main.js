6e3d75340ecea91b6de6d422b78335ed95ce4330
MichaelCodes <michael.lang3303@gmail.com>
2026-01-11 16:03:38 -0600

    Constructor Practice

--- Class_Materials/class26-materials/class26-materials/objects-constructor-fun/js/main.js
+++ Class_Materials/class26-materials/class26-materials/objects-constructor-fun/js/main.js
@@ -1,1 +1,18 @@
 //Create a constructor with 4 properties and 3 methods
+function CarConstructor(model,color,doors,wheels){
+    this.model = model 
+    this.color = color 
+    this.doors = doors 
+    this.wheels = wheels 
+    this.honk = function(){
+        console.log('Honking')
+    }
+    this.drive = function(){
+        console.log('Driving')
+    }
+    this.steer = function(){
+        console.log('steering')
+    }
+}
+
+let redRocket = new CarConstructor('cruze', 'red', '4', '26in')
