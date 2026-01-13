51a4a9f35fb245b8c905c613c40c229f930c9445
MichaelCodes <michael.lang3303@gmail.com>
2026-01-12 19:51:32 -0600

    Drinks Carousel

--- Class_Materials/class26-materials/class26-materials/objects-shots/js/main.js
+++ Class_Materials/class26-materials/class26-materials/objects-shots/js/main.js
@@ -1,15 +1,27 @@
 //The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
 document.querySelector('button').addEventListener('click', getDrink);
 
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function dataLoop(data){
+    for(let i = 0; i<data.length;i++){
+            console.log(data[i]);
+            document.querySelector('h2').innerText = data[i].strDrink;
+            document.querySelector('img').src = data[i].strDrinkThumb;
+            document.querySelector('h3').innerText = data[i].strInstructions;
+            await delay(1000);
+        }
+}
+
 function getDrink(){
     let drink = document.querySelector('input').value
     fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`)
     .then(res => res.json())
     .then(data => {
         console.log(data.drinks[0])
-        document.querySelector('h2').innerText = data.drinks[0].strDrink;
-        document.querySelector('img').src = data.drinks[0].strDrinkThumb;
-        document.querySelector('h3').innerText = data.drinks[0].strInstructions;
+        dataLoop(data.drinks);
     })
     .catch(err =>{
         console.log(`error ${err}`)
