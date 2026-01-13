b7acb00b15d9eceb880d8d32dac76a90c1fea107
MichaelCodes <michael.lang3303@gmail.com>
2026-01-12 20:17:09 -0600

    NASA API Integration

--- Class_Materials/class26-materials/class26-materials/objects-nasa/js/main.js
+++ Class_Materials/class26-materials/class26-materials/objects-nasa/js/main.js
@@ -1,1 +1,19 @@
 //The user will enter a date. Use that date to get the NASA picture of the day from that date! https://api.nasa.gov/
+//https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY
+
+document.querySelector('button').addEventListener('click',getImage);
+
+function getImage(){
+    let date = document.querySelector('input').value;
+    console.log(date);
+    fetch(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${date}`)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data.url)
+        document.querySelector('h2').innerText = `Date: ${data.date}`;
+        document.querySelector('img').src = data.url;
+    })
+    .catch(err =>{
+        console.log(`error ${err}`)
+    });
+}
