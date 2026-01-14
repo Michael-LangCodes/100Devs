11a37714c6a96342a17fe94faeee9fd8496ec543
MichaelCodes <michael.lang3303@gmail.com>
2026-01-13 18:25:10 -0600

    Netflix Changes

--- Class_Materials/class27-materials/class27-materials/objects-netflix-tv-shows/js/main.js
+++ Class_Materials/class27-materials/class27-materials/objects-netflix-tv-shows/js/main.js
@@ -1,3 +1,11 @@
 //Create a Netflix TV Show class with a constructor that makes Netflix TV Shows with 4 properties and 3 methods
-
+class NetflixShow{
+    constructor(title,location,genre,episodes){
+        this.title = title
+        this.location = location 
+        this.genre = genre 
+        this.episodes = episodes 
+        this.
+    }
+}
 
