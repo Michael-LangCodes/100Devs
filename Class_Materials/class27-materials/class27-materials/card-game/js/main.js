a561ec1b5a274fc01fe9467ae021d141fb127c76
MichaelCodes <michael.lang3303@gmail.com>
2026-01-14 18:59:48 -0600

    Card Game Showing Cards

--- Class_Materials/class27-materials/class27-materials/card-game/js/main.js
+++ Class_Materials/class27-materials/class27-materials/card-game/js/main.js
@@ -1,17 +1,26 @@
 //Example fetch using pokemonapi.co
-document.querySelector('button').addEventListener('click', getFetch)
+let deckId = ''
 
-function getFetch(){
-  const choice = document.querySelector('input').value
-  const url = 'https://pokeapi.co/api/v2/pokemon/'+choice
+document.querySelector('button').addEventListener('click', drawTwo)
 
-  fetch(url)
-      .then(res => res.json()) // parse response as JSON
-      .then(data => {
-        console.log(data)
-      })
-      .catch(err => {
+fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
+  .then(res=>res.json())
+  .then(data =>{
+    deckId = data.deck_id;
+    console.log(deckId);
+  })
+  .catch(err => {
           console.log(`error ${err}`)
       });
+
+function drawTwo(){
+  const url = `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`;
+  fetch(url)
+    .then(res => res.json())
+    .then(data=>{
+      console.log(data.cards[0].value, data.cards[0].suit);
+      document.querySelector('#player1').src = data.cards[0].image;
+      document.querySelector('#player2').src = data.cards[1].image;
+    })
 }
 
