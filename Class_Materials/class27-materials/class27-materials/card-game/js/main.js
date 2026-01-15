1c1d2d2b6d29bfebc8a16f23ff22a8a06dd009cd
MichaelCodes <michael.lang3303@gmail.com>
2026-01-14 22:15:39 -0600

    War Function Working

--- Class_Materials/class27-materials/class27-materials/card-game/js/main.js
+++ Class_Materials/class27-materials/class27-materials/card-game/js/main.js
@@ -18,11 +18,17 @@ console.log(deckId)
 
 document.querySelector('button').addEventListener('click', drawTwo)
 
+function reshuffle(){
+  const url = `https://deckofcardsapi.com/api/deck/${deckId}/shuffle/`;
+  fetch(url)
+}
+
 function drawTwo(){
   const url = `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`;
   fetch(url)
     .then(res => res.json())
     .then(data=>{
+      console.log(data)
       document.querySelector('#player1card1').src = data.cards[0].image;
       document.querySelector('#player2card1').src = data.cards[1].image;
 
@@ -35,22 +41,40 @@ function drawTwo(){
         document.querySelector('h3').innerText = 'Player 2 Wins';
       }else{
         document.querySelector('h3').innerText = 'Time For War!!!';
+        console.log('Starting War!!!');
+        startWar();
       }
     })
 }
 
 function startWar(){
-  let player1Pile = {};
-  let player2Pile = {};
+  let player1Pile = [];
+  let player2Pile = [];
   const url = `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`;
   for(let i = 0; i < 3; i++){
     fetch(url)
     .then(res => res.json())
     .then(data=>{
-      document.querySelector('#player1').src = data.cards[0].image;
-      document.querySelector('#player2').src = data.cards[1].image;
+      player1Pile.push(data.cards[0])
+      player2Pile.push(data.cards[1])
+      document.querySelector(`#player1card${i+1}`).src = data.cards[0].image
+      document.querySelector(`#player2card${i+1}`).src = data.cards[1].image
+      //document.querySelector('#player1').src = data.cards[0].image;
+      //document.querySelector('#player2').src = data.cards[1].image;
   })
   }
+  console.log(`This is Player 1 In War ${player1Pile}`);
+
+
+  let player1WarVal = convertToNum(player1Pile[player1Pile.length-1]).value;
+  let player2WarVal = convertToNum(player2Pile[player2Pile.length-1]).value;
+  if(player1WarVal > player2WarVal){
+        document.querySelector('h3').innerText = 'Player 1 Wins';
+      }else if(player1WarVal < player2WarVal){
+        document.querySelector('h3').innerText = 'Player 2 Wins';
+      }else{
+        document.querySelector('h3').innerText = 'Time For War Again!!!';
+      }
 }
 
 function convertToNum(val){
