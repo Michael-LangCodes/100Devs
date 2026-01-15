a5a9945292cfbd833b58be36d9e21c3084a623d0
MichaelCodes <michael.lang3303@gmail.com>
2026-01-14 18:43:19 -0600

    Score Keeping

--- Class_Materials/class27-materials/class27-materials/local-storage-botScore-button/js/main.js
+++ Class_Materials/class27-materials/class27-materials/local-storage-botScore-button/js/main.js
@@ -1,3 +1,12 @@
 //Create a button that adds 1 to a botScore stored in localStorage 
+document.querySelector('button').addEventListener('click', addAnother)
+if(!localStorage.getItem('botScore')){
+    localStorage.setItem('botScore',0)
+}
 
+function addAnother(){
+  let botScoreVal = Number(localStorage.getItem('botScore'));
+  botScoreVal +=1;
+  localStorage.setItem('botScore', botScoreVal);
+}
 
