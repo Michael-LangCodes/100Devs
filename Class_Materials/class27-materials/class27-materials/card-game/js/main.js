6c679d340a644ca16d277459b035b0ce3de4b1d5
MichaelCodes <michael.lang3303@gmail.com>
2026-01-14 19:28:54 -0600

    Added Local Storage of Deck ID

--- Class_Materials/class27-materials/class27-materials/card-game/js/main.js
+++ Class_Materials/class27-materials/class27-materials/card-game/js/main.js
@@ -1,24 +1,28 @@
 //Example fetch using pokemonapi.co
-let deckId = ''
-
-document.querySelector('button').addEventListener('click', drawTwo)
-
-fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
+if (!localStorage.getItem('storedDeckID')){
+  let deckId = '';
+  fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
   .then(res=>res.json())
   .then(data =>{
     deckId = data.deck_id;
     console.log(deckId);
+    localStorage.setItem('storedDeckID',deckId);
   })
   .catch(err => {
           console.log(`error ${err}`)
       });
+}
+
+let deckId = localStorage.getItem('storedDeckID')
+console.log(deckId)
+
+document.querySelector('button').addEventListener('click', drawTwo)
 
 function drawTwo(){
   const url = `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`;
   fetch(url)
     .then(res => res.json())
     .then(data=>{
-      console.log(data.cards[0].value, data.cards[0].suit);
       document.querySelector('#player1').src = data.cards[0].image;
       document.querySelector('#player2').src = data.cards[1].image;
 
