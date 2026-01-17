fd79f1d29b827b0c7793f577406ddf09ff8e4f3b
MichaelCodes <michael.lang3303@gmail.com>
2026-01-16 21:01:24 -0600

    Creation of li and append to ul

--- Class_Materials/class28-materials/class28-materials/dnd/js/main.js
+++ Class_Materials/class28-materials/class28-materials/dnd/js/main.js
@@ -8,6 +8,17 @@ function getFetch(){
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
+       //document.querySelector('h3').innerText = data.classes[0].name;
+       //document.querySelector('h4').innerText = data.subclasses[0].name;
+       data.subclasses.forEach(element => {
+        console.log(element.name);
+        //create an li
+        const li = document.createElement('li');
+        //add text to li
+        li.textContent = element.name;
+        //append the li to the ul
+        document.querySelector('ul').appendChild(li);
+       });
        
       })
       .catch(err => {
