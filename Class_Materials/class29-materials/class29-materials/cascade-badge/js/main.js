69dff793f03abc2e0d9bf49fb8dad798cd98d3f3
MichaelCodes <michael.lang3303@gmail.com>
2026-01-18 16:33:19 -0600

    Index Array Creation Improvements

--- Class_Materials/class29-materials/class29-materials/cascade-badge/js/main.js
+++ Class_Materials/class29-materials/class29-materials/cascade-badge/js/main.js
@@ -32,8 +32,7 @@ console.log(compareSquareToCube(array1,array2));
 // [22, -6, 32, 82, 9, 25] =>  [-6, 32, 25]
 // [68, -1, 1, -7, 10, 10] => [-1, 10]
 function arrayIndexMultiple(arr){
-    const newArr = arr.filter((num,i) => num % i === 0)
-    return newArr
+    return newArr = arr.filter((num,i) => num % i === 0);
 }
 test1 = [22, -6, 32, 82, 9, 25]
 test2 = [68, -1, 1, -7, 10, 10]
