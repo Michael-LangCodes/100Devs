6d28b640fd37913e25a8706675f7dd5fe29c59f0
MichaelCodes <michael.lang3303@gmail.com>
2026-01-18 21:01:31 -0600

    Pokemon API Integration for Types

--- Class_Materials/class29-materials/class29-materials/rainbow-badge/js/main.js
+++ Class_Materials/class29-materials/class29-materials/rainbow-badge/js/main.js
@@ -1,45 +1,46 @@
 //Example fetch using pokemonapi.co
 document.querySelector('button').addEventListener('click', getFetch)
 
-function getFetch(){
-  const poke1 = document.querySelector('#poke1').value
-  const poke2 = document.querySelector('#poke2').value
+async function getFetch(){
+  const poke1 = document.querySelector('#poke1').value.toLowerCase()
+  const poke2 = document.querySelector('#poke2').value.toLowerCase()
   const url = 'https://pokeapi.co/api/v2/pokemon/'+poke1
   const url2 = 'https://pokeapi.co/api/v2/pokemon/'+poke2
-  let pokeStore = []
-  let pokeImg = []
 
-  fetch(url)
-      .then(res => res.json()) // parse response as JSON
-      .then(data => {
+  try{
+  const res1 = await fetch(url);
+  const data1 = await res1.json();
+  
+  const res2 = await fetch(url2);
+  const data2 = await res2.json();
+  
+  const poke1types = data1.types.map(t => t.type.name)
+  const poke2types = data2.types.map(t => t.type.name)
 
-        pokeStore.push(data.types[0].type.name)
-        pokeImg.push(data.sprites.front_shiny)
-        
-        fetch(url2)
-        .then(res => res.json()) // parse response as JSON
-        .then(data => {
+  const typeShared = poke1types.some(type =>
+      poke2types.includes(type)
+    )
+    console.log('Shared type:', typeShared);
 
-          pokeStore.push(data.types[0].type.name)
-          pokeImg.push(data.sprites.front_shiny)
-      
-          if((pokeStore[0] === "grass" && pokeStore[1] === 'water')){
-            document.querySelector('#pokeImg1').src = pokeImg[0]
-            document.querySelector('#pokeImg2').src = pokeImg[1]
-            document.querySelector('h2').innerText = " 2x > "
-          }
-        })
-        .catch(err => {
-            console.log(`error ${err}`)
-        });
+    document.querySelector('p').innerHTML = `${poke1} types: ${poke1types} <br> ${poke2} types: ${poke2types}`
+  // for (let i = 0; i < data1.types.length; i++){
+  //   console.log(data1.types[i].type.name)
+  //   poke1types.push(data1.types[i].type.name);
+  // }
+  // for (let i = 0; i < data2.types.length; i++){
+  //   console.log(data2.types[i].type.name);
+  //   poke2types.push(data2.types[i].type.name);
+  //     }
 
+  }
+   catch (err) {
+    console.error(err)
+   }
 
-      })
-      .catch(err => {
-          console.log(`error ${err}`)
-      });
-
-
-
-      
+    // const typeShared = poke1types.some(element => poke2types.includes(element));
+    // console.log(poke1types);
+    // console.log(poke2types);
+    // console.log(typeShared);
 }
+
+      
