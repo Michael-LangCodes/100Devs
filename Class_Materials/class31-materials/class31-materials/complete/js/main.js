7615d649d77ee5c95b353dd356c267ae41644147
MichaelCodes <michael.lang3303@gmail.com>
2026-01-19 22:34:31 -0600

    Farm Object Bird Creation

--- Class_Materials/class31-materials/class31-materials/complete/js/main.js
+++ Class_Materials/class31-materials/class31-materials/complete/js/main.js
@@ -35,12 +35,26 @@ class Cat extends Animal{
         console.log(`${this.name} meows`)
     }    
 }
+class Bird extends Animal{
+    constructor(name,breed){
+        super(name)
+        this._breed = breed
+    }
+    get breed(){
+        return this.breed
+    }
+    speak(){
+        super.speak()
+        console.log(`${this.name} chirps`)
+    }
+}
 
 let simba = new Dog('Simba','Shepard')
 let machi = new Dog('The Machine','Pitbull')
 let salem = new Cat('Salem', 'American Shorthair')
+let pik = new Bird('Pik', 'Parrot')
 
-let farm = [simba,machi,salem]
+let farm = [simba,machi,salem,pik]
 
 for( a of farm ){
     a.speak()
