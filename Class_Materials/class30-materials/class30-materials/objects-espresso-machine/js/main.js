2dfce36e45a5bd148b0beab6a00d70528ef87722
MichaelCodes <michael.lang3303@gmail.com>
2026-01-19 19:18:02 -0600

    Espresso Machine Object

--- Class_Materials/class30-materials/class30-materials/objects-espresso-machine/js/main.js
+++ Class_Materials/class30-materials/class30-materials/objects-espresso-machine/js/main.js
@@ -1,1 +1,18 @@
 //Create an espresso machine constructor that makes machines with 4 properties and 3 methods
+class EspressoMachine{
+    constructor(color,size,shape,temp){
+        this.color = color,
+        this.size = size,
+        this.shape = shape,
+        this.temp = temp
+    }
+    displayTemp(){
+        console.log(`This is the temp: ${this.temp}`);
+    }
+    makeCoffee(){
+        console.log('Making coffee');
+    }
+    cleanMachine(){
+        console.log('Cleanniiiinnnnggg')
+    }
+}
