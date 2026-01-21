1b3c0b07b30a4c6ae39538398df61977c2b16ce5
MichaelCodes <michael.lang3303@gmail.com>
2026-01-20 19:07:21 -0600

    Class Practice

--- Class_Materials/class31-materials/class31-materials/complete/js/main.js
+++ Class_Materials/class31-materials/class31-materials/complete/js/main.js
@@ -1,3 +1,43 @@
+// class Animal{
+//     constructor(name){
+//         this._name = name
+//     }
+//     get name(){
+//         return this._name
+//     }
+//     speak(){
+//         console.log(`${this._name} makes a sound`)
+//     }
+// }
+
+// class Dog extends Animal{
+//     constructor(name,breed){
+//         super(name)
+//         this._breed = breed
+//     }
+//     get breed(){
+//         return this._breed
+//     }
+//     speak(){
+//         super.speak()
+//         console.log(`${this.name} barks`)
+//     }
+// }
+
+// class Cat extends Animal{
+//     constructor(name,breed){
+//         super(name)
+//         this._breed = breed
+//     }
+//     get breed(){
+//         return this._breed
+//     }
+//     speak(){
+//         super.speak()
+//         console.log(`${this.name} meows`)
+//     }
+// }
+
 class Animal{
     constructor(name){
         this._name = name
