9a38ee698e2a8516d129f2de48876248a45856d4
MichaelCodes <michael.lang3303@gmail.com>
2026-01-20 19:43:41 -0600

    Class and Extension Commit

--- Class_Materials/class31-materials/class31-materials/objects-child-class/js/main.js
+++ Class_Materials/class31-materials/class31-materials/objects-child-class/js/main.js
@@ -1,2 +1,37 @@
 //Create an a class and extend it - Can be anything you would like it to be! 
 
+class Vehicle{
+    constructor(color,model,brand){
+        this._color = color
+        this._model = model
+        this._brand = brand
+    }
+    get color(){
+        return this._color
+    }
+    get model(){
+        return this._model
+    }
+    get brand(){
+        return this._brand
+    }
+    description(){
+        console.log(`It is a ${this._color} ${this._brand} ${this._model}`)
+    }
+}
+
+class Truck extends Vehicle{
+    constructor(color,model,brand,drive){
+        super(color,model,brand)
+        // super(model)
+        // super(brand)
+        this._drive = drive
+    }
+    description(){
+        super.description()
+        console.log(`It also has ${this._drive} wheel drive`)
+    }
+}
+
+let tacoma = new Truck('red','tacoma','toyota','4')
+tacoma.description()
