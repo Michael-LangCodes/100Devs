91126f0a64c399443a1138109427db21a6984589
MichaelCodes <michael.lang3303@gmail.com>
2026-02-18 21:51:31 -0600

    Page Loading

--- Class_Materials/class32-materials/node-crash-course/index.js
+++ Class_Materials/class32-materials/node-crash-course/index.js
@@ -6,12 +6,14 @@ const fs = require('fs');
 const server = http.createServer((req, res) => {
 
     if (req.url === '/') {
+        fs.readFile(path.join(__dirname, 'public', 'index.html'), (err,content)=>{
         res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end('<h1>Home</h1>');
-    } else {
-        res.writeHead(404, { 'Content-Type': 'text/plain' });
-        res.end('404 Not Found');
-    }
+        res.end('<h1>Home</h1>');}
+)}    if (req.url === '/about') {
+        fs.readFile(path.join(__dirname, 'public', 'index.html'), (err,content)=>{
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end('<h1>Home</h1>');}
+)}
 
 });
 
