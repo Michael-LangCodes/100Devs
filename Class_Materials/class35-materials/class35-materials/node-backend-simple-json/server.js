93ff9bf96553095c674f470e1380ea2546e01b5a
MichaelCodes <michael.lang3303@gmail.com>
2026-02-20 10:46:34 -0600

    Node Practice

--- Class_Materials/class35-materials/class35-materials/node-backend-simple-json/server.js
+++ Class_Materials/class35-materials/class35-materials/node-backend-simple-json/server.js
@@ -4,30 +4,24 @@ const url = require('url');
 const querystring = require('querystring');
 const figlet = require('figlet')
 
+const consistentHTML = function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      res.end();
+    }
+
 const server = http.createServer((req, res) => {
   const page = url.parse(req.url).pathname;
   const params = querystring.parse(url.parse(req.url).query);
   console.log(page);
   if (page == '/') {
-    fs.readFile('index.html', function(err, data) {
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(data);
-      res.end();
-    });
+    fs.readFile('index.html', consistentHTML);
   }
   else if (page == '/otherpage') {
-    fs.readFile('otherpage.html', function(err, data) {
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(data);
-      res.end();
-    });
+    fs.readFile('otherpage.html', consistentHTML);
   }
   else if (page == '/otherotherpage') {
-    fs.readFile('otherotherpage.html', function(err, data) {
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(data);
-      res.end();
-    });
+    fs.readFile('otherotherpage.html', consistentHTML);
   }
   else if (page == '/api') {
     if('student' in params){
