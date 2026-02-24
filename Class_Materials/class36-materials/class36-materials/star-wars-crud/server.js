8ff647398d779322b8a821fa5c31c8be72d7c76a
MichaelCodes <michael.lang3303@gmail.com>
2026-02-23 18:52:55 -0600

    Posting Updates

--- Class_Materials/class36-materials/class36-materials/star-wars-crud/server.js
+++ Class_Materials/class36-materials/class36-materials/star-wars-crud/server.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 
+// Make sure you place this before your CRUD handlers!
+app.use(express.urlencoded({ extended: true }))
+
 app.listen(3000, function () {
   console.log('listening on 3000')
 })
@@ -13,6 +16,10 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
   // Note: __dirname is the current directory you're in. Try logging it and see what you get!
   // Mine was '/Users/zellwk/Projects/demo-repos/crud-express-mongo' for this app.
+})
+
+app.post('/quotes', (req, res) => {
+  console.log(req.body)
 })
 
 console.log('May Node be with you')
