7ee1b2b4f3404001a5932a3fe125d7b77e31379a
MichaelCodes <michael.lang3303@gmail.com>
2026-02-24 19:32:51 -0600

    Simple API Request

--- Class_Materials/class38-materials/class38-materials/rap-names-api/server.js
+++ Class_Materials/class38-materials/class38-materials/rap-names-api/server.js
@@ -2,8 +2,35 @@ const express = require('express');
 const app = express();
 const PORT = 8000;
 
+const rappers = {
+    '21 Savage':{
+    'age': 29,
+    'birthName': 'Sheyaa Bin Abraham-Joseph',
+    'birthLocation': 'London, England'
+    },
+    'Chance the Rapper':{
+    'age': 29,
+    'birthName': 'Chancellor Bennett',
+    'birthLocation': 'Chicago, Illinois'
+    },
+    'Dylan':{
+    'age': 29,
+    'birthName': 'Dylan',
+    'birthLocation': 'Dylan'
+    },
+}
+
 app.get('/', (request, response)=>{
     response.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/:rapperName', (request,response)=>{
+    const rapperName = request.params.rapperName
+    if (rappers[rapperName]){
+        response.json(rappers[rapperName])
+    }else{
+        response.json(rappers['Dylan'])
+    }
 })
 
 app.listen(PORT, ()=>{
