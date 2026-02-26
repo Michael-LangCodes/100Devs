a2e98c83ca82a0b6153fde0d8608245e90e906d9
MichaelCodes <michael.lang3303@gmail.com>
2026-02-25 19:48:01 -0600

    API Conditional

--- Class_Materials/class39-materials/rap-names-api/server.js
+++ Class_Materials/class39-materials/rap-names-api/server.js
@@ -2,9 +2,37 @@ const express = require('express');
 const app = express()
 const PORT = 8000;
 
+const rappers = {
+'21 savage':{
+    'age': 29,
+    'birthName': 'Sheyaa Bin Abraham-Joseph',
+    'birthLocation': 'London, England'
+    },
+'chance the rapper':{
+    'age': 29,
+    'birthName': 'Chancellor Bennett',
+    'birthLocation': 'Chicago, Illinois'
+    },
+'unknown':{
+    'age': 'unknown',
+    'birthName': 'unknown',
+    'birthLocation': 'unknown'
+    }
+}
+
 app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/index.html')
 });
+
+app.get('/api/:name', (req,res)=>{
+    const rapperName = req.params.name.toLocaleLowerCase()
+    console.log(rappers[rapperName])
+    if(rappers[rapperName]){
+        res.json(rappers[rapperName])
+    }else{
+        res.json(rappers['unknown'])
+    }
+})
 
 app.listen(PORT, ()=>{
     console.log(`The server is now running on port ${PORT}! Beetta Go Catch It!`)
