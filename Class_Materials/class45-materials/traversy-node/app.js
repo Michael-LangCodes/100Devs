c328add8647da53a88a985457a41bbaca9643505
MichaelCodes <michael.lang3303@gmail.com>
2026-03-01 09:54:33 -0600

    Added Morgan Logging

--- Class_Materials/class45-materials/traversy-node/app.js
+++ Class_Materials/class45-materials/traversy-node/app.js
@@ -1,14 +1,20 @@
 const express = require('express');
+const morgan = require('morgan');
 const dotenv = require('dotenv');
-const connectDB = require('./config/db')
+const connectDB = require('./config/db');
 
 //Load config
-dotenv.config({path: './config/config.env'})
+dotenv.config({path: './config/config.env'});
 
-connectDB()
+//Connecting the DB
+connectDB();
 
 const app = express();
 
-const PORT = process.env.PORT || 5000
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'))
+}
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
