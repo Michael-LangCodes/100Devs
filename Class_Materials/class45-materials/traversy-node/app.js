a283c51b811b66547f81057edd3c587bae655130
MichaelCodes <michael.lang3303@gmail.com>
2026-03-02 21:53:55 -0600

    Initial Handlebar Setup

--- Class_Materials/class45-materials/traversy-node/app.js
+++ Class_Materials/class45-materials/traversy-node/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
+const exphbs = require('express-handlebars');
 const connectDB = require('./config/db');
+const { extname } = require('node:path');
 
 //Load config
 dotenv.config({path: './config/config.env'});
@@ -11,9 +13,14 @@ connectDB();
 
 const app = express();
 
+//Logging
 if(process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'))
 }
+
+//Handlebars
+app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
+app.set('view engine', '.hbs');
 
 const PORT = process.env.PORT || 5000;
 
