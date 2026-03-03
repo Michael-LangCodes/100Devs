dc7290534ed592504a22cdaa8771e007d7b6b2ca
MichaelCodes <michael.lang3303@gmail.com>
2026-03-02 22:11:32 -0600

    Views Working

--- Class_Materials/class45-materials/traversy-node/app.js
+++ Class_Materials/class45-materials/traversy-node/app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
-const exphbs = require('express-handlebars');
+const { exphbs } = require('express-handlebars');
 const connectDB = require('./config/db');
 const { extname } = require('node:path');
+const { engine } = require("express-handlebars");
 
 //Load config
 dotenv.config({path: './config/config.env'});
@@ -18,8 +19,11 @@ if(process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'))
 }
 
+//Routes
+app.use('/', require('./routes/index'))
+
 //Handlebars
-app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
+app.engine('.hbs', engine({defaultLayout: 'main', extname: '.hbs'}));
 app.set('view engine', '.hbs');
 
 const PORT = process.env.PORT || 5000;
