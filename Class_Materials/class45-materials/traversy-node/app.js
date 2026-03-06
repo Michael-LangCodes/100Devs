ccd83844ce12bdc96f0a1b5cd98621df8dc0052c
MichaelCodes <michael.lang3303@gmail.com>
2026-03-05 19:44:29 -0600

    Updated Time Stamps

--- Class_Materials/class45-materials/traversy-node/app.js
+++ Class_Materials/class45-materials/traversy-node/app.js
@@ -29,8 +29,13 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+//Handlebars Helper
+const {formatDate } = require('./helpers/hbs')
+
 // Handlebars
-app.engine('.hbs', engine({ defaultLayout: 'main', extname: '.hbs' }));
+app.engine('.hbs', engine({ helpers: {
+    formatDate,
+}, defaultLayout: 'main', extname: '.hbs' }));
 app.set('view engine', '.hbs');
 
 // Sessions
