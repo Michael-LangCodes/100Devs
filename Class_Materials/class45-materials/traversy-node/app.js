80baa193b64f4c5b85805873160c1e4c8a4fd7c2
MichaelCodes <michael.lang3303@gmail.com>
2026-03-05 19:38:40 -0600

    stories.js

--- Class_Materials/class45-materials/traversy-node/app.js
+++ Class_Materials/class45-materials/traversy-node/app.js
@@ -20,6 +20,10 @@ connectDB();
 
 const app = express();
 
+//Body Parser
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
 // Logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
@@ -47,6 +51,7 @@ app.use(passport.session());
 // Routes (AFTER passport + sessions)
 app.use('/', require('./routes/index'));
 app.use('/auth', require('./routes/auth'));
+app.use('/stories', require('./routes/stories'));
 
 // Static folder
 app.use(express.static(path.join(__dirname, 'public')));
