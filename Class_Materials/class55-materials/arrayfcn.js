cb4fe49954ee23bab94e51b32eb83722416acc00
MichaelCodes <michael.lang3303@gmail.com>
2026-03-08 23:14:10 -0500

    forEach and find

--- Class_Materials/class55-materials/arrayfcn.js
+++ Class_Materials/class55-materials/arrayfcn.js
@@ -5,10 +5,26 @@ const items = [
     { name: 'Book', price: 5},
     { name: 'Phone', price: 500},
     { name: 'Computer', price: 1000},
+    { name: 'Keyboard', price: 25},
 ]
 
 const filteredItems = items.filter((item)=>{
     return item.price<=100
 })
 
-console.log(filteredItems)
+const mapItems = items.map((item)=>{
+    return item.name
+})
+
+const found = items.find((item)=>{
+    return item.name = 'Book'
+})
+
+items.forEach((item)=>{
+    console.log(item.name)
+})
+
+console.log(items);
+console.log(filteredItems);
+console.log(mapItems);
+console.log(found);
